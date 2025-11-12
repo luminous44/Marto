08057bbd2f4fa3c5e3951baef2f23b5bb4a11ac9
luminous44 <aklemasultanakhi@gmail.com>
2025-11-12 18:37:54 +0600

    added get all product api request

--- cmd/main.go
+++ cmd/main.go
@@ -6,16 +6,14 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"github.com/luminous44/Marto/config"
 	"github.com/luminous44/Marto/routes"
 )
 
 func main() {
-    config.Connection()
 	r := mux.NewRouter()
     routes.RegisterRoutes(r)
-	fmt.Println("server running on server :8080")
-	if err := http.ListenAndServe(":8080",r); err != nil {
+	fmt.Println("server running on server :9000")
+	if err := http.ListenAndServe(":9000",r); err != nil {
 		fmt.Println("Failed to start server",err)  
 		log.Fatal(err)
 		
